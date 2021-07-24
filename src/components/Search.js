3884162c36ac13bc3fb8d4d997c4c1f0810b1518
Luan Barbosa <luanqbarbosa@gmail.com>
2021-07-24 09:50:18 -0300

    Search component results list render

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,6 @@ import axios from 'axios';
 const Search = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
-
-    console.log(results);
     
     useEffect(() => {
         const search = async () => {
@@ -27,6 +25,27 @@ const Search = () => {
             search();
         }
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );
+    });
     
     return (
         <div>
@@ -39,6 +58,9 @@ const Search = () => {
                         onChange={(e) => setTerm(e.target.value)}
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
